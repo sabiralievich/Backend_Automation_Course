e8c33ef6ad81e9fa97892bda01d945e02417b0a1
sabiralievich <bulat.bliznyuk@gmail.com>
2022-02-11 13:02:09 +0500

    LESSON-4 Делаем тесты на пост запрос: https://spoonacular.com/food-api/docs#Add-to-Meal-Plan ResponseSpecification added

---
+++ src/test/java/gb/backendtestingautomation/lesson4/AddToMealPlanTest.java
@@ -0,0 +1,30 @@
+package gb.backendtestingautomation.lesson4;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.ResponseSpecification;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class AddToMealPlanTest {
+    ResponseSpecification responseSpecification = null;
+
+    @BeforeEach
+    void beforeTest() {
+        responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectStatusLine("HTTP/1.1 200 OK")
+                .expectContentType(ContentType.JSON)
+                .expectResponseTime(Matchers.lessThan(5000L))
+                .expectHeader("Access-Control-Allow-Credentials", "true")
+                .build();
+    }
+
+    @Test
+    void addToMealPlanTest() {
+
+    }
+}
